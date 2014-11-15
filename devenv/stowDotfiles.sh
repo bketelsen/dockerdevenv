84b3ab594bb99fe3d236a07c5e64f9fe0a6e7dca
Brian Ketelsen <brian@xor.exchange>
2014-11-15 13:51:35 -0500

    almost there

--- devenv/stowDotfiles.sh
+++ devenv/stowDotfiles.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
 
 set -e
-cd /home/$1
-git clone https://github.com/$1/dotfiles
-cd /home/$1/dotfiles
+cd /home/${1}
+echo `pwd`
+echo $1
+echo `whoami`
+git clone https://github.com/${1}/dotfiles /home/${1}/dotfiles
+cd /home/${1}/dotfiles
 #get fonts
 git submodule update --init --recursive
 
 #install fonts
-/home/$1  /dotfiles/vendor/powerline-fonts/install.sh
+/home/${1}/dotfiles/_vendor/powerline-fonts/install.sh
 
 # put vim config in place
 stow -vv vim
