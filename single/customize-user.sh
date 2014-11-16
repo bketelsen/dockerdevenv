36385e2e6ae4c5649dfa6e8b768169b5c87e68fd
Brian Ketelsen <brian@xordataexchange.com>
2014-11-16 16:31:44 -0500

    forgot this one

--- single/customize-user.sh
+++ single/customize-user.sh
@@ -16,7 +16,4 @@ stow -vv i3
 stow -vv config
 
 
-chown -R bketelsen /home/bketelsen && chgrp -R bketelsen /home/bketelsen
-chmod 400 /home/bketelsen/.vnc/passwd
-chmod +x /home/bketelsen/.vnc/xstartup
-chsh -s /usr/bin/zsh bketelsen
+
