84e1287d16d29a27044083dc1e7afa8d87fa6e81
Brian Ketelsen <bketelsen@gmail.com>
2014-11-16 20:51:20 +0000

    noninteractive

--- single/base-services.sh
+++ single/base-services.sh
@@ -4,9 +4,8 @@
 # abort on errors 
 set -e
 # don't expect a tty
-export DEBIAN_FRONTEND noninteractive
-apt-get update && apt-get install -y openssh-server \
+DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y openssh-server \
       ca-certificates curl unzip tar \
       zsh stow   
-apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+DEBIAN_FRONTEND=noninteractive apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
--- single/dev-packages.sh
+++ single/dev-packages.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 # Install development packages
-export DEBIAN_FRONTEND noninteractive
-apt-get update && apt-get install -y python-pip build-essential git-core mercurial bzr python-dev ctags
-apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y python-pip build-essential git-core mercurial bzr python-dev ctags
+DEBIAN_FRONTEND=noninteractive apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 easy_install supervisor
 
 
