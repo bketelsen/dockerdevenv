dfe3848b589ac63131b1b84a1e9c483930731f20
Brian Ketelsen <bketelsen@gmail.com>
2014-11-17 04:15:31 +0000

    adding cmake for ycm

--- single/dev-packages.sh
+++ single/dev-packages.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # Install development packages
-DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y python-pip build-essential git-core mercurial bzr python-dev ctags
+DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y python-pip build-essential git-core mercurial bzr python-dev ctags cmake
 DEBIAN_FRONTEND=noninteractive apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 easy_install supervisor
 
