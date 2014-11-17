b557d2f6ad06f2758bd84ffd6f04484412e7a3da
Brian Ketelsen <bketelsen@gmail.com>
2014-11-16 21:18:19 -0500

    adding chrome

--- single/install-ui.sh
+++ single/install-ui.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 # Install Window Manager, Xwindows, etc.
 
-DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y i3 terminator feh dmenu dunst connman-ui rox-filer hexchat tightvncserver vim-gnome
+DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y i3 terminator feh dmenu dunst connman-ui rox-filer hexchat tightvncserver vim-gnome libxss1 libappindicator1 libindicator7 libnspr4 libnss3 libappindicator1
 DEBIAN_FRONTEND=noninteractive apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb /src/chrome.deb
-dpkg -i /src/chrome.deb
+cd /src/
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
+dpkg -i google-chrome*.deb 
 
 
  
