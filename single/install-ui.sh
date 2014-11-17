31602ca918d9baa16b5b41119dc60100030cf1da
Brian Ketelsen <bketelsen@gmail.com>
2014-11-17 07:53:14 -0500

    swap ff for chrome

--- single/install-ui.sh
+++ single/install-ui.sh
@@ -1,12 +1,9 @@
 #! /bin/bash
 # Install Window Manager, Xwindows, etc.
 
-DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y i3 terminator feh dmenu dunst connman-ui rox-filer hexchat tightvncserver vim-gnome libxss1 libappindicator1 libindicator7 libnspr4 libnss3 libappindicator1
+DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y i3 terminator feh dmenu dunst connman-ui rox-filer hexchat tightvncserver vim-gnome libxss1 libappindicator1 libindicator7 libnspr4 libnss3 libappindicator1 firefox
 DEBIAN_FRONTEND=noninteractive apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-cd /src/
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
-dpkg -i google-chrome*.deb 
 
 
  
