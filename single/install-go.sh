fa272e35dcb8f7be81cd18114beddc06979b6740
Brian Ketelsen <bketelsen@gmail.com>
2014-12-03 13:00:31 -0500

    Update install-go.sh

--- single/install-go.sh
+++ single/install-go.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-mkdir -p /usr/local/go && curl -Ls https://storage.googleapis.com/golang/go1.3.3.linux-amd64.tar.gz | tar xvzf - -C /usr/local/go --strip-components=1
+mkdir -p /usr/local/go && curl -Ls https://storage.googleapis.com/golang/go1.4rc2.linux-amd64.tar.gz | tar xvzf - -C /usr/local/go --strip-components=1
 
